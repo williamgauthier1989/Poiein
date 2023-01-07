729820fa8f3ffba5ef4caa8b2b026fbeb90c3d71
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2023-01-07 01:44:07 +0100

    Ajoute le comportement du Poiein
    Déplacements
    Proto d'un pathfinding dynamique

---
+++ Assets/Scripts/GroundBehavior.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GroundBehavior : MonoBehaviour
+{
+    public NavMeshSurface surface;
+    private int _rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        surface.BuildNavMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ChangeLayer());
+    }
+
+    private IEnumerator ChangeLayer()
+    {
+        yield return new WaitForSeconds(5f);
+        _rand = Random.Range(1, 3);
+        Debug.Log("rand : " + _rand);
+        if (_rand == 1)
+        {
+            if (gameObject.layer != 8)
+            {
+                gameObject.layer = 8;
+                surface.BuildNavMesh();
+                Debug.Log("Change to Ground");
+            }
+        }
+        else
+        {
+            if (gameObject.layer != 6)
+            {
+                gameObject.layer = 6;
+                surface.BuildNavMesh();
+                Debug.Log("Change to Obstacle");
+            }
+        }
+    }
+}
